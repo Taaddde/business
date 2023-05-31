87000831f1a112bb8f3b5190be383b00a64f8a35
taaddde <taaddde@hotmail.com>
2023-05-31 13:50:10 -0300

    [FT] Firsts tests

--- apps/api/test/app.e2e-spec.ts
+++ apps/api/test/app.e2e-spec.ts
@@ -1,24 +1,59 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { ApiModule } from './../src/api.module';
+import { AppModule } from './../src/app.module';
+import { UserService } from '../src/components/user/user.service';
 
 describe('ApiController (e2e)', () => {
   let app: INestApplication;
+  let userService: UserService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [ApiModule],
+      imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    userService = moduleFixture.get<UserService>(UserService);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('/register (POST)', () => {
+    it('should register a new user successfully', () => {
+      jest.spyOn(userService, 'checkExists').mockResolvedValue(null);
+      jest.spyOn(userService, 'saveUser').mockResolvedValue({ email: 'bruce@wayne.com' });
+      return request(app.getHttpServer())
+        .post('/users/register')
+        .send({email: 'bruce@wayne.com', password: 'imbatman123'})
+        .expect(201)
+    });
+
+    it('should return an error if email is already taken', () => {
+      jest.spyOn(userService, 'checkExists').mockResolvedValue({ email: 'bruce@wayne.com'});
+      return request(app.getHttpServer())
+        .post('/users/register')
+        .send({email: 'bruce@wayne.com', password: 'imbatman123'})
+        .expect(409)
+    });
+
+    it('should return an error if password is not a string', () => {
+      jest.spyOn(userService, 'checkExists').mockResolvedValue(null);
+      jest.spyOn(userService, 'saveUser').mockResolvedValue({ email: 'ta' });
+      return request(app.getHttpServer())
+        .post('/users/register')
+        .send({email: 'tadeeee', password: 1})
+        .expect(201)
+    });
+
+    it('should return an error if password is not a string', () => {
+      jest.spyOn(userService, 'checkExists').mockResolvedValue(null);
+      jest.spyOn(userService, 'saveUser').mockResolvedValue({ email: 'ta' });
+      return request(app.getHttpServer())
+        .post('/users/register')
+        .send({email: 'tadeeee', password: 'ta'})
+        .expect(201)
+    });
   });
 });
+
